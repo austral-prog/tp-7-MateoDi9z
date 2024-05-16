8761a56dff8b4fd698d87177fa7c63d26e5c2bb5
MateoDi9z <dzmateozd@gmail.com>
2024-05-16 20:36:40 -0300

    cleaner

--- loops_and_print.py
+++ loops_and_print.py
@@ -2,8 +2,8 @@ def enumerate_list(lista: list[str]):
     newArray = []
 
     for el in lista:
-        if not el:
-            continue
+        if not el: continue
+
         newArray.append(f"{len(newArray)}. {el}")
 
     return newArray
@@ -12,8 +12,8 @@ def enumerate_backwards(lista: list[str]):
     newArray = []
 
     for el in lista:
-        if not el:
-            continue
+        if not el: continue
+
         newArray.append(f"{len(newArray)}. {el[::-1]}")
 
     return newArray
